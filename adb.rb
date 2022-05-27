1ccfa94facc234837c90ad0da38920832b0058f5
Aki <light299792@gmail.com>
2022-05-28 00:31:20 +0900

    add adb.rb

---
+++ adb.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+#
+
+# eagle vision の Eagle.db を ShotView にコピーする
+# adb を事前に接続しておく必要あり
+#
+
+require 'optparse'
+
+exit unless ARGV.options {|opt|
+  opt.on( '-v', '--virbose' )   { |v| $OPT_v = v }
+  opt.on( '-n', 'suppress' )    { |v| $OPT_n = v }
+  opt.on( '-i', '--interactive' )  { |v| $OPT_i = v }
+  opt.on( '--get' )  { |v| $OPT_get = v }
+  opt.on( '--put' )  { |v| $OPT_put = v }
+  opt.on( '--cat' )  { |v| $OPT_cat = v }
+  opt.parse!(ARGV)
+}
+
+def do_cmd( cmd )
+  IO.popen( cmd, "r+" ) {|io|
+    while !io.eof
+      puts io.gets.gsub( /\r/, '' )
+      yield line  if block_given?
+    end
+  }
+  unless $?.success?
+    puts "cmd: '#{cmd}'"
+    raise "do_cmd() Error"
+  end
+end
+
+DIC = 'kotonoha.txt'
+DIR = "/sdcard/Documents/Kotonoha"
+
+if $OPT_get
+  do_cmd( "adb pull #{DIR}/#{DIC} ." )
+elsif $OPT_cat
+  do_cmd( "adb shell cat #{DIR}/#{DIC}" )
+elsif $OPT_put
+  do_cmd( "adb push #{DB} #{DIR}/#{DIC}" )
+else
+  do_cmd( "adb shell ls -la #{DIR}/#{DIC}" )
+end
+
+
+  
